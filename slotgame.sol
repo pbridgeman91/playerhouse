dab800d43f6ceeacbe6e679a9627e87b96652945
pbridgeman91 <pbridgeman@me.com>
2025-06-18 07:40:37 -0700

    Update slotgame.sol

--- slotgame.sol
+++ slotgame.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 
 /* ────────────────────────── Interfaces ────────────────────────── */
 
-
 interface IERC20 {
     function transfer(address to, uint256 amount) external returns (bool);
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
